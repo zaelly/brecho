16a0535dde046c02d56edd843cfac1a845f4516f
Zaelly Barbosa <zazabarbosa2@gmail.com>
2025-06-06 20:00:55 -0300

    chat vendedor + ux criado

--- admin/src/components/ChatSeller/ChatSeller.jsx
+++ admin/src/components/ChatSeller/ChatSeller.jsx
@@ -1,27 +1,48 @@
 import { useState } from 'react'
 import './ChatSeller.css'
-import heroImage from '../../assets/hero.png';
+import Join from '../Join/Join';
 
 const ChatSeller = () => {
     const [allChats, setAllChats] = useState([])
+    const [newMsg, setNewMsg] = useState(false)
+    const [selectedChatId, setSelectedChatId] = useState(null);
+    const [clickMsg, setClickMsg] = useState(false)
+
   return (
     <div className="ChatSeller">
         {/* mostra os users msg */}
         <div className='chatsList'>
-            <p>teste</p>
-            <div className="userChat">
-                {/* quando clica no chat ele 
-                traz a conversa */}
-                <div className="inputSeller">
-                <img src={heroImage}/>   
-                <p>Zaelly</p>  
-                </div>
-            </div> 
+            {/* quando clica no chat ele 
+            traz a conversa */}
+            {allChats > 0 ? (allChats.map((chats)=>(
+                <div key={chats.id} 
+                    className={`inputSeller ${selectedChatId === chats.id ? 'active' : ''}`}
+                    onClick={() => {setSelectedChatId(chats.id); setClickMsg(true)}} >
+                    <div className="userChat">
+                        <img src={chats.image}/>   
+                        <p>{chats.name}</p>  
+                    </div>
+                    {/* se tiver mensagem nova */}
+                    <div className={newMsg ? "newMsg" : "hide"}>
+                        <i className="fa-solid fa-circle-exclamation"></i>
+                    </div>
+                </div>  
+            ))) : (
+                <h1>Sem mensagens</h1>
+            )}
         </div>
         {/* lado q mostra as mensagens */}
         <div className="msg-box">
-            <h1>Nenhuma mensagem</h1>
-        </div>
+            {clickMsg ? 
+                <Join 
+                    id={item._id} 
+                    name={item.name} 
+                    image={item.image} 
+                /> 
+                : 
+                <h3>Nada por aqui!</h3>
+            }
+        </div> 
     </div>
   )
 }
--- admin/src/components/Join/Join.jsx
+++ admin/src/components/Join/Join.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
+import './Join.css'
 
 const Join = (props) => {
-    const JoinChat = ()=>{
-        console.log('Ola')
+
+  const [mensage, setMensage] = useState("")
+    const sendMsg = (e)=>{
+      console.log(mensage)
+      setMensage("");
     }
   return (
-    <div className="Join" onClick={JoinChat}>
+    <div className="Join" id={props.id}>
         {/* quando clica em um chat ao lado aparece 
         as mensagens desse chat */}
         <div className="chatBox-header">
-            {/* imagem do user */}
-            <img src="" alt="" />
-            <p>Nome do usuario</p>
+            <div className="userChat">
+                <img src={props.image}/>   
+                <p>{props.name}zaelly</p>  
+            </div>
         </div> 
+        <div className="chatMessages">
+          {/* Aqui entram as mensagens do chat */}
+        </div>
+
+        <div className="joinMsg">
+          <div className="footer-sendmsg">
+            <input type="text" name="sendMsg" value={mensage} 
+            onChange={(e)=> setMensage(e.target.value)} 
+            className='sendmsg' placeholder='Digite uma mensagem'/>
+            <i className="fa-regular fa-circle-right" onClick={sendMsg}></i>
+          </div>
+        </div>
     </div>
   )
 }
